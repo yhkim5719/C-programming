5ff60d77d375058c67e3f9758df09f3dca40e179
Coursera Learner <nobody@nowhere.nul>
2018-09-15 09:35:35 +0000

    Did assigment 6.

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,14 +16,40 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+typedef struct{
+  int x;
+  int y;
+  int width;
+  int height;
+} rectangle;
 
 rectangle canonicalize(rectangle r) {
-  //WRITE THIS FUNCTION
+  if (r.width < 0){
+    r.x = r.x + r.width;
+    r.width = -1 * r.width;
+  };
+  if (r.height < 0){
+    r.y = r.y + r.height;
+    r.height = -1 * r.height;
+  };
+    //WRITE THIS FUNCTION
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
-  //WRITE THIS FUNCTION
+  if ((r2.x + r2.width <= r1.x)||
+      (r1.y + r1.height <= r2.y)||
+      (r1.x + r1.width <= r2.x)||
+      (r2.y + r2.height <= r1.y)||
+      ((r1.x + r1.width < r2.x + r2.width) && (r2.x <= r1.x) && (r1.y >= r2.y) && (r1.y + r1.height <= r2.y + r2.height))){
+    printf("no intersection");
+  }else {
+    r1.x = max(r1.x, r2.x);
+    r1.y = max(r1.y, r2.y);
+    r1.width = min(r1.x + r1.width, r2.x + r2.width);
+    r1.height = min(r1.y + r1.height, r2.y + r2.height);
+  }
+    
+    //WRITE THIS FUNCTION
   return r1;
 }
 
