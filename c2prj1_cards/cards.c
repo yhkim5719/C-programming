2e842e42c149df5e82f2cc0e9e260ebba48b7cd4
Coursera Learner <yhkim5719@gmail.com>
2018-09-24 01:37:21 +0000

    4th

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -77,7 +77,7 @@ char suit_letter(card_t c) {
 }
 
 void print_card(card_t c) {
-  printf("%c, %c", value_letter(c), suit_letter(c));
+  printf("%c%c", value_letter(c), suit_letter(c));
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
