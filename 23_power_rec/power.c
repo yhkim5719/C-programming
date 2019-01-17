7d305777890d1fa6918d7c6a7b8872480dc9df37
Coursera Learner <yhkim5719@gmail.com>
2019-01-17 04:58:04 +0000

    ??

--- 23_power_rec/power.c
+++ 23_power_rec/power.c
@@ -1,4 +1,8 @@
-unsigned power (unsigned x, unsigned y) {
+unsigned power_helper (unsigned x, unsigned y) {
 	if (y == 0) return 1;
-	return x * power (x, y - 1);
+	return x * power_helper (x, y - 1);
+	}
+
+unsigned power (unsigned x, unsigned y) {
+	return power_helper(x, y);
 	}
