90a3453bd1c2397d660f6adb0149cbc48953046b
Coursera Learner <yhkim5719@gmail.com>
2019-01-17 00:25:57 +0000

    ??

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -4,7 +4,7 @@
 unsigned power (unsigned x, unsigned y);
 void run_check (unsigned x, unsigned y, unsigned expected_ans) {
 	if (power(x, y) != expected_ans) {
-		printf("Test failed.");
+		printf("Test failed.\n");
 		exit(EXIT_FAILURE);
 		}
 	}
@@ -16,7 +16,9 @@ int main(void){
 	run_check (1.1, 2, 1.21);
 	run_check (0, 0, 0);
 	run_check (0, 1, 0);
-	run_check (-1.1, 2, 1.21);
+	run_check (-2, 1, -2);
+	run_check (4, 0.5, 2);
+	run_check (4, -0.5, 0.5);
 		
 	return EXIT_SUCCESS;
 	}
