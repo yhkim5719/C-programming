a303789c05de4136f9a3cf29f9b18ffd9eb8b9ca
Coursera Learner <yhkim5719@gmail.com>
2019-01-17 01:22:02 +0000

    ???

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -7,18 +7,25 @@ void run_check (unsigned x, unsigned y, unsigned expected_ans) {
 		printf("Test failed.\n");
 		exit(EXIT_FAILURE);
 		}
+	else {
+		return;
+		}
 	}
 
 int main(void){
 	run_check (2, 0, 1);
 	run_check (1, 2, 1);
 	run_check (-2, 0, 1);
-	run_check (1.1, 2, 1.21);
-	run_check (0, 0, 0);
+//	run_check (1.1, 2, 1.21);
+	run_check (0, 0, 1);
 	run_check (0, 1, 0);
 	run_check (-2, 1, -2);
-	run_check (4, 0.5, 2);
-	run_check (4, -0.5, 0.5);
-		
+	run_check (-2, -1, -0.5);
+//	run_check (4, 0.5, 2);
+//	run_check (4, -0.5, 0.5);
+	run_check (2, -1, 0.5);	
+	run_check (-1, 2, 1);
+//	run_check (-1, 1, -1);
+	run_check (-1, -1, -1);
 	return EXIT_SUCCESS;
 	}
