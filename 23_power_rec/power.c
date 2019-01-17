7987734859dfd2912e265ba1dd68c367ca55a0fb
Coursera Learner <yhkim5719@gmail.com>
2019-01-17 05:02:38 +0000

    tail rec??

--- 23_power_rec/power.c
+++ 23_power_rec/power.c
@@ -1,8 +1,8 @@
-unsigned power_helper (unsigned x, unsigned y) {
-	if (y == 0) return 1;
-	return x * power_helper (x, y - 1);
+unsigned power_helper (unsigned x, unsigned y, unsigned ans) {
+	if (y == 0) return ans;
+	return power_helper (x, y - 1, ans * x);
 	}
 
 unsigned power (unsigned x, unsigned y) {
-	return power_helper(x, y);
+	return power_helper(x, y, 1);
 	}
