7613bf811258086b278047ed1ca04d519bb19950
Coursera Learner <yhkim5719@gmail.com>
2019-06-27 20:36:16 +0000

    2 funtions done.

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -3,11 +3,20 @@
 #include <assert.h>
 #include "deck.h"
 void print_hand(deck_t * hand){
-
+	for (int i = 0; i < hand->n_cards; ++i) {
+//		printf("%c%c ", value_letter(*((*hand).cards[i])), suit_letter(*((*hand).cards[i])));
+		print_card(*(hand->cards[i]));
+		printf(" ");
+	}
 }
 
 int deck_contains(deck_t * d, card_t c) {
-  return 0;
+	for (int i = 0; i < d->n_cards; ++i) {
+		if ((*(d->cards[i])).value == c.value && (*(d->cards[i])).suit == c.suit) {
+			return 1;
+		}
+	}
+  	return 0;
 }
 
 void shuffle(deck_t * d){
