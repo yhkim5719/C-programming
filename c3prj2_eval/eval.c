c517ebb55c1b4c2c318c190c21c5142695ab6d8b
Coursera Learner <yhkim5719@gmail.com>
2019-07-03 19:06:57 +0000

    .

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -129,9 +129,9 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
 		return eval_hand2.ranking - eval_hand1.ranking;
 	} else {
 		for (int i = 0; i < 5; ++i) {
-			diff = (eval_hand1.cards[i])->value - (eval_hand2.cards[i])->value;
+			diff = (eval_hand2.cards[i])->value - (eval_hand1.cards[i])->value;
 			if (diff == 0) {
-				diff = (eval_hand2.cards[i])->suit - (eval_hand1.cards[i])->suit; 
+				diff = (eval_hand1.cards[i])->suit - (eval_hand2.cards[i])->suit; 
 				if (diff != 0) {
 					return diff;
 				}
