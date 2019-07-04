e87b1048a27049c21a2e7f870ead574472ba64fb
Coursera Learner <yhkim5719@gmail.com>
2019-07-04 00:38:22 +0000

    .

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -69,17 +69,17 @@ int is_straight_at(deck_t * hand, size_t index, suit_t fs) {
 			}
 		}
 		if (n_length >= 5) {return 1;}
-		n_length = 1;
-		ref_value = 6;
+		n_length = 0;
+		ref_value = 5;
 		for (size_t i = index; i < hand->n_cards-1; ++i) {
 			if (hand->cards[i+1]->suit != fs || hand->cards[i+1]->suit != fs) {continue;}
-			if (hand->cards[i+1]->value == ref_value-1 && hand->cards[i+1]->suit ==fs) {
+			if (hand->cards[i+1]->value == ref_value && hand->cards[i+1]->suit ==fs) {
 				ref_value--;
 				n_length++;
 			}
 		}
-		if (n_length == 5 && 
-			hand->cards[index]->value == 14) 		// TODO check the value
+		if (n_length == 4 
+			&& hand->cards[index]->value == 14) 		// TODO check the value
 //			hand->cards[index]->suit == fs)
 			{return -1;}
 		return 0;
@@ -93,16 +93,16 @@ int is_straight_at(deck_t * hand, size_t index, suit_t fs) {
 			}
 		}
 		if (n_length >= 5) {return 1;}
-		n_length = 1;
-		ref_value = 6;
+		n_length = 0;
+		ref_value = 5;
 		for (size_t i = index; i < hand->n_cards-1; ++i) {
-			if (hand->cards[i+1]->value == ref_value-1) {
+			if (hand->cards[i+1]->value == ref_value) {
 				ref_value--;
 				n_length++;
 			}
 		}
-		if (n_length == 5 && 
-			hand->cards[index]->value == 14)		// TODO check the value
+		if (n_length == 4 
+			&& hand->cards[index]->value == 14)		// TODO check the value
 			{return -1;}
 		return 0;
 	}
