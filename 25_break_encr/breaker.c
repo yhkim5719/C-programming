7983892ebb26533785d0247d36287dbdd66b149c
Coursera Learner <yhkim5719@gmail.com>
2019-07-15 02:33:34 +0000

    .

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -112,15 +112,15 @@ int lower_char (char c) {
 }
 
 int count_freq(FILE * f) {
-//	int count[26] = {0};
+//	if (f == NULL) {return 0;}
 	int count[26] = {0};
 	int c;
 	c = fgetc(f);
-	if (c == EOF) {EXIT_FAILURE;}
+//	if (c == EOF) {EXIT_FAILURE;}
 	while (c != EOF) {
+//	while ((fgetc(f)) != EOF) {
 		if (isalpha(c)) {
 			c = lower_char(c);
-//			tolower(c);
 			c -='a';
 			count[c]++;
 		}
@@ -134,12 +134,13 @@ int count_freq(FILE * f) {
 		if (max < count[i]) {
 			max = count[i];
 			idx = i;
-//			printf("max = %d\n", max);
-//			printf("idx = %d\n", idx);
+			printf("max = %d\n", max);
+			printf("idx = %d\n", idx);
 		}
 	}
-	printf (/*"idx = */ "%d\n", idx - 'e' + 'a');
-	return idx - 'e' + 'a';
+	printf (/*"idx = */ "%d\n", idx /*- 'e' + 'a'*/);
+	if (idx == 0 && count[0] == 0) {return 0;}
+	return idx /*+ 'e' - 'a'*/;
 }
 	
 void decrypt(FILE * f, int key) {    
@@ -147,7 +148,6 @@ void decrypt(FILE * f, int key) {
   while ((c = fgetc(f)) != EOF) {        
     if (isalpha(c)) {            
       c = lower_char(c);            
-//      c = tolower(c);            
       c -= 'a';            
       c -= key;            
       c %= 26;            
@@ -163,14 +163,9 @@ int main(int argc, char ** argv) {
     fprintf(stderr,"Usage: breaker inputFileName\n");    
     return EXIT_FAILURE;  
   }  
-//  int key = atoi(argv[1]);  
-//  if (key == 0) {    
-//    fprintf(stderr,"Invalid key (%s): must be a non-zero integer\n", argv[1]);    
-//    return EXIT_FAILURE;  
-//  }  
-  FILE * f = fopen(argv[1], "r+");  
+  FILE * f = fopen(argv[1], "r");  
   if (f == NULL) {    
-    perror("Could not open file");    
+    fprintf(stderr, "Could not open file");    
     return EXIT_FAILURE;  
   }
   int key = count_freq(f);  
@@ -181,74 +176,3 @@ int main(int argc, char ** argv) {
   }  
   return EXIT_SUCCESS;
 }
-
-/*
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-int count_freq (FILE* input) {
-	int count[26] = {0};
-	int c;
-	c = fgetc(input);
-	if (c == EOF) {EXIT_FAILURE;}
-	while (c != EOF) {
-		if (isalpha(c)) {
-			tolower(c);
-			c -='a';
-			count[c]++;
-		}
-		c = fgetc(input);
-	}
-	int max = count[0];
-	int idx = 0;
-	for (int i = 0; i < 26; i++) {
-		printf("count[%d] = %d\n", i, count[i]);	//TODO
-		if (max < count[i+1]) {
-			max = count[i+1];
-			idx = i+1;
-		}
-	}
-//	printf ("idx = %d\n", idx);
-	return 'e' - 'a' + idx;
-}
-
-
-void decrypt (FILE* input, int key) {
-	int c;
-	while ((c = fgetc(input)) != EOF) {
-		if (isalpha(c)) {
-//			tolower(c);
-//			c -= 'a';
-			c += key;
-//			c %= 26;
-//			c += 'a';
-			printf ("%c", c);
-		}
-	}
-	return;
-}
-
-*/
-
-/*
-int main (int argc, char** argv) {
-	if (argc != 2) {
-		fprintf (stderr, "USAGE: \"./breaker input.txt");
-		EXIT_FAILURE;
-	}
-	FILE* f = fopen (argv[1], "r"); 
-	if (f == NULL) {
-		perror ("cannot open file.");
-//		fprintf (stderr, "cannot open file.");
-		EXIT_FAILURE;
-	}
-	int key = count_freq (f);
-	printf("%d\n", key);
-	decrypt (f, key);
-		
-	return 0;
-}
-
-*/
