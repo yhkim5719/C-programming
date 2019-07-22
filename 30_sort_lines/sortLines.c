ade575ae60338baf26f71eea8679e519aa8c1263
Coursera Learner <yhkim5719@gmail.com>
2019-07-22 04:57:55 +0000

    .

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -20,73 +20,6 @@ void sortData(char ** data, size_t count) {
 	}
 }
 
-/*
-int main(int argc, char ** argv) {
-  
-  if (argc == 0) {
-    fprintf(stderr,"Not enough arguments \n");
-    return EXIT_FAILURE;
-  }
-  else if (argc == 1){
-    //read from stdio
-    char** data = NULL;
-    char* line = NULL;
-    size_t sz = 0;
-    int i = 0; 
-    while(getline(&line, &sz, stdin) >= 0){
-      data = realloc(data, (i + 1) * sizeof(*data));
-      data[i] = line;
-      line = NULL;
-      i++;
-    }
-    free(line);
-    sortData(data, i);
-    for (int j=0; j < i; j++){
-      printf("%s", data[j]);
-      free(data[j]);
-    }
-    free(data);
-  }
-  else {
-    for( int k = 1; k < argc; k++){
-      char** data = NULL;
-      char* line = NULL;
-      size_t sz = 0;
-      int i = 0; 
-      FILE * f = fopen(argv[k], "r");
-      if (f == NULL){
-        perror("Could not open file");
-        return EXIT_FAILURE;
-      }
-      while(getline(&line, &sz, f) >= 0){
-        data = realloc(data,(i + 1) * sizeof(*data));
-        data[i] = line;
-        line = NULL;
-        i++;
-      }
-      free(line);
-      sortData(data, i);
-      for(int j = 0;j < i; j++){
-        printf("%s", data[j]);
-        free(data[j]);
-      }
-      free(data);
-      if (fclose(f) != 0) {
-	perror("Failed to close the input file!");
-	return EXIT_FAILURE;
-      }
-      //read from file 
-    }
-  
-  
-    //WRITE YOUR CODE HERE!
-  
-    return EXIT_SUCCESS;
-  }
-}
-
-*/
-
 void proc (FILE* input) {
 	char** data = NULL;
 	
@@ -108,22 +41,13 @@ void nonArgProc () {
 	
 	size_t sz = 0;
 	int i = 0;
-	char** data = (char**) malloc(sizeof(*data));
+	char** data = NULL;
 	char* line = NULL;
 	while((getline(&line, &sz, stdin)) >= 0) {
-/*		if (sz == 0) {
-			line = NULL;
-			free(line);
-			sortData(data, i);
-			free(data);
-			return;
-*///		}
 		data = realloc(data, (i + 1) * sizeof(*data));
 		data[i] = line;
-//		printf("data[%d] = %s", i, data[i]);
-//		printf("line = %s", line);
-		i++;
 		line = NULL;
+		i++;
 	}			// read all lines
 	free(line);
 	sortData(data, i);	// sort & print TODO a func
