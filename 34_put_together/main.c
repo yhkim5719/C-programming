f3894fbbb6876885c7baa5cc3bcd888a007070bf
Coursera Learner <yhkim5719@gmail.com>
2019-07-28 22:40:50 +0000

    .

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -14,10 +14,9 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
 	
 	counts_t* c = createCounts();
 	char* key = NULL;
-	size_t sz;
+	size_t sz = 0;
 	while (getline(&key, &sz, f) != -1) {
 		key[strlen(key) - 1] = '\0';
-//		printf("%s\n", lookupValue(kvPairs, key));
 		addCount(c, lookupValue(kvPairs, key));
 		key = NULL;
 	}
@@ -44,7 +43,7 @@ int main(int argc, char ** argv) {
 
 		FILE* f = fopen(outName, "w+");    //open the file named by outName (call that f)
 		printCounts(c, f);    //print the counts from c into the FILE f
-	//	fclose(f);    //close f
+//		fclose(f);    //close f
 		free(outName);    //free the memory for outName and c
 		freeCounts(c);
 	}
