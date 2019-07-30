c5689273a3b7b6300b93750a0bf7b1d319637c1e
Coursera Learner <yhkim5719@gmail.com>
2019-07-30 04:16:00 +0000

    .'

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -14,7 +14,7 @@ void print_hand(deck_t * hand){
 int deck_contains(deck_t * d, card_t c) {
 	if (d->n_cards != 0) {
 		for (int i = 0; i < d->n_cards; ++i) {
-			if ((*(d->cards[i])).value == c.value && (*(d->cards[i])).suit == c.suit) {
+			if (d->cards[i]->value == c.value && d->cards[i]->suit == c.suit) {
 				return 1;
 			}
 		}
@@ -57,7 +57,7 @@ void add_card_to(deck_t* deck, card_t c) {
 }
 
 card_t* add_empty_card(deck_t* deck) {
-	card_t* e_card = malloc(*e_card);
+	card_t* e_card = malloc(sizeof(*e_card));
 	e_card.value = 0;
 	e_card.suit = 0;
 	add_card_to(deck, e_card);
@@ -66,11 +66,35 @@ card_t* add_empty_card(deck_t* deck) {
 }
 
 deck_t* make_deck_exclude(deck_t* excluded_cards) {
-	
+	deck_t* tmp_deck = malloc(sizeof(*tmp_deck));
+	tmp_deck->n_cards = 0;
+	card_t tmp;
+	for (int i = 0; i < 52; i++) {
+		tmp = card_from_num(i);
+		if (deck_contains(excluded_cards, tmp) == 0) {
+			add_card_to(tmp_deck, tmp);
+		}
+	}
+	return tmp_deck;
 }
 
 deck_t* build_remaining_deck(deck_t** hands, size_t n_hands) {
+	deck_t* tmp_deck = malloc(sizeof(*tmp_deck));
+	for (size_t i = 0; i < n_hands; i++) {
+		for (int j = 0; j < hands[i]->n_cards; j++) {
+			add_card_to(tmp_deck, hands[i]->cards[j]);
+		}
+	}
+	deck_t* remaining_deck = make_deck_exclude(tmp_deck);
+	free_deck(tmp_deck);
+	
+	return remaining_deck;
 }
 
 void free_deck(deck_t* deck) {
+	for (int i = 0; i < deck->n_cards; i++) {
+		free(deck->cards[i]);
+	}
+	free(deck->cards);
+	free(deck);
 }
