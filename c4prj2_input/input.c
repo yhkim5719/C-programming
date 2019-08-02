da51ac31f42c45734c3741538a3c93775ecca278
Coursera Learner <yhkim5719@gmail.com>
2019-08-02 23:41:22 +0000

    .

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -45,7 +45,7 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
 	size_t sz = 0;
 	while (getline(&hand, &sz, f) >= 0) {
 		input = realloc(input, (n_hand + 1) * sizeof(*input));
-//		input[n_hand] = malloc(sizeof(deck_t));
+		input[n_hand] = malloc(sizeof(deck_t));
 		input[n_hand] = hand_from_string(hand, fc);
 		hand = NULL;
 		n_hand++;
