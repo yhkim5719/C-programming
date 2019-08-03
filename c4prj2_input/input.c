667a65362590908c2ec37e36d5114c9420e222e9
Coursera Learner <yhkim5719@gmail.com>
2019-08-03 02:44:37 +0000

    .

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -1,60 +1,3 @@
-#include<string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include "deck.h"
-#include "cards.h"
-#include "future.h"
-deck_t * hand_from_string(const char * str, future_cards_t * fc){
-  deck_t* deck=malloc(sizeof(*deck));
-  deck->cards = NULL;
-  deck->n_cards=0;
-  for(int i=0;i<strlen(str);i++){
-    if((str[i] == '\n')||(str[i] == ' '))continue;
-    else{
-      if(str[i] == '?'){
-        i++;
-        char num[strlen(str)];
-        int n=0;
-        while(!((str[i] == '\n')||(str[i] == ' '))) {
-          num[n]=str[i];
-          i++;n++;}
-        num[n]='\0';
-        add_future_card(fc,atoi(num),add_empty_card(deck)) ;
-
-      }
-      else{
-        card_t x = card_from_letters(str[i],str[i+1]);
-        add_card_to(deck,x);
-        i++;}
-    }
-  }
-  if (deck->n_cards < 5 ){
-    return NULL;
-    fprintf(stderr,"aaaaaa");}
-  return deck;
-}
-deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
-  deck_t **arr=NULL;
-  size_t n_hand=0;
-  char *line=NULL;
-  size_t sz=0;
-  while(getline(&line,&sz,f)>=0){
-    arr=realloc(arr,(n_hand+1)*sizeof(*arr));
-    deck_t*deck=hand_from_string(line,fc);
-    if (deck == NULL)continue;
-    arr[n_hand]=deck;
-    n_hand ++;
-  }
-  free(line);
-  *n_hands=n_hand;
-  return arr;
-}
-
-
-
-
-/*
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
@@ -64,10 +7,10 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
 	deck_t* tmp = malloc(sizeof(*tmp));
 	tmp->cards = NULL;
 	tmp->n_cards = 0;
-
 	for(int i = 0; i < strlen(str); i++) {
-    		if(str[i] == '\n' || str[i] == ' ' || str[i] == '\0'){continue;}
-    		else {
+    		if(str[i] == '\n' || str[i] == ' ' || str[i] == '\0'){
+			continue;
+		} else {
       			if(str[i] == '?') {
         			i++;
         			char num[strlen(str)];
@@ -105,10 +48,10 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
 	char* hand = NULL;
 	size_t sz = 0;
 	while (getline(&hand, &sz, f) >= 0) {
-//		if(hand[0] == '\n') {continue;}
+		if(hand[0] == '\n') {continue;}
 		input = realloc(input, (n_hand + 1) * sizeof(*input));
 		deck_t* tmp = hand_from_string(hand, fc); // 
-		if (tmp == NULL) {continue;}
+//		if (tmp == NULL) {continue;}
 		input[n_hand] = tmp; 
 		n_hand++;
 	}
@@ -117,7 +60,9 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
 	
 	return input;
 }
-*/
+
+
+
 /*
 
 - deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc);
