de3fd871aa4ed7cad61b8ad17c40c1ba6f196e55
Coursera Learner <yhkim5719@gmail.com>
2019-08-03 01:34:41 +0000

    .

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -24,7 +24,7 @@ void add_future_card (future_cards_t* fc, size_t index, card_t* ptr) {
   }
 */
 	if (index >= fc->n_decks) {
-		fc->decks = realloc(fc->decks, index * sizeof(*fc->decks));
+		fc->decks = realloc(fc->decks, (index + 1) * sizeof(*fc->decks));
 		while (index >= fc->n_decks) {
 			fc->decks[fc->n_decks].cards = NULL;
 			fc->decks[fc->n_decks].n_cards = 0;
