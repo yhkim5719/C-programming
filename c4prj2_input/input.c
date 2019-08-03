64dc779c13d430ff2cc1287edf332b79fa146477
Coursera Learner <yhkim5719@gmail.com>
2019-08-03 06:26:37 +0000

    .

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -38,17 +38,17 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
 }
 
 deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
-	if (f == NULL) {
-		perror("Invalid file");
-		return NULL;
-	}
+//	if (f == NULL) {
+//		perror("Invalid file");
+//		return NULL;
+//	}
 	deck_t** input = NULL;
 	size_t n_hand = 0;
 
 	char* hand = NULL;
 	size_t sz = 0;
 	while (getline(&hand, &sz, f) >= 0) {
-		if(hand[0] == '\n') {continue;}
+//		if(hand[0] == '\n') {continue;}
 		input = realloc(input, (n_hand + 1) * sizeof(*input));
 		deck_t* tmp = hand_from_string(hand, fc); // 
 		if (tmp == NULL) {continue;}
