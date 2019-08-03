e5db61a3254cf00e757a0339f100f89a73997221
Coursera Learner <yhkim5719@gmail.com>
2019-08-03 04:06:07 +0000

    .

--- c4prj2_input/test.c
+++ c4prj2_input/test.c
@@ -4,6 +4,10 @@
 #include "input.h"
 
 int main(int argc, char** argv) {
-	FILE* f = fopen(
+	if (argc != 3) {return EXIT_FAILURE;}
+	FILE* f = fopen(argv[1], "r");
+	size_t* n;
+	future_cards_t* fc;
+	deck** input = read_input(f, n, fc);
 
 }
