99345babc0445644579794a1e6ed7963670d104d
Coursera Learner <yhkim5719@gmail.com>
2019-08-03 07:45:40 +0000

    .

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -13,12 +13,12 @@ void add_future_card (future_cards_t* fc, size_t index, card_t* ptr) {
 		}
 	}	
 	fc->decks[index].cards = realloc(fc->decks[index].cards, (fc->decks[index].n_cards + 1) * sizeof(deck_t));// *(fc->decks[index].cards)));
- 	fc->decks[index].cards[fc->decks[index].n_cards - 1] = ptr;
+ 	fc->decks[index].cards[fc->decks[index].n_cards] = ptr;
  	fc->decks[index].n_cards++;
 }
 
 void future_cards_from_deck (deck_t* deck, future_cards_t* fc) {
-	if (deck->n_cards > fc->n_decks) {
+	if (deck->n_cards < fc->n_decks) {
 		perror("Need more cards in deck");
 		return;
 	}
@@ -27,6 +27,7 @@ void future_cards_from_deck (deck_t* deck, future_cards_t* fc) {
 		for (int j = 0; j < fc->decks[i].n_cards; j++) {
 			fc->decks[i].cards[j]->value = deck->cards[i]->value;
 			fc->decks[i].cards[j]->suit = deck->cards[i]->suit;
+			print_card(deck->cards[i]); // TODO
 		}
 	}
 }
