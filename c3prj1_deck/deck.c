a599ea64a74a8b9b3b356ed18781c29d5d7ebbad
Coursera Learner <yhkim5719@gmail.com>
2019-08-03 06:17:41 +0000

    .

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -52,8 +52,8 @@ void assert_full_deck(deck_t * d) {
 
 void add_card_to(deck_t* deck, card_t c) {
 	card_t* cc = malloc(sizeof(card_t));
-	cc.value = c.value;
-	cc.suit = c.suit;
+	cc->value = c.value;
+	cc->suit = c.suit;
 	deck->n_cards++;
 	deck->cards = realloc(deck->cards, (deck->n_cards) * sizeof(*deck->cards));
 	deck->cards[deck->n_cards - 1] = cc; 
