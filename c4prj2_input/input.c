05acd7cdceceba89640cca2a6eecd3248d2f7b5a
Coursera Learner <yhkim5719@gmail.com>
2019-08-06 19:14:00 +0000

    .

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -13,7 +13,6 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
       			if(str[i] == '?') {
         			char num[5];
         			int j = 0;
-//				num[j++] = ' ';
         			i++;
         			while(isdigit(str[i])) {
           				num[j++] = str[i++];
@@ -49,51 +48,8 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
 		input[n_hand] = hand_from_string(line, fc); 
 		n_hand++;
 	}
-//	size_t n = n_hand;
-//	while (n > 0) {
-//		print_hand(input[n_hand - n]);
-//		printf("\n\n");
-//		n--;
-//	}
 	*n_hands = n_hand;
 	free(line);
 	
 	return input;
 }
-
-
-
-/*
-
-- deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc);
-  
-   This function reads the input from f.  Recall that the input
-   file has one hand per line (and that we represent a hand
-   with a deck_t).  You should allocate a deck_t for each hand
-   and place it into an array of pointers to deck_ts, which is your
-   answer.
-   This function needs to tell its caller how many hands it read.
-   We could return a struct, but we are going to do this a
-   different way: it will fill in *n_hands with the number
-   of hands.  This is a bit different, but you have seen it
-   before: this is how getline "returns" the string it
-   read and the size of the space allocated to that string.
-
-   As you read the input, if you encounter future cards
-   (?0, ?1, ...), you should use add_empty_card to
-   create a placeholder in the hand, and then add_future_card
-   to make sure you will update it correctly when you draw
-   later.  Also remember that you wrote add_card_to
-   earlier in this course, as well as card_from_letters
-   in Course 2.  These will both be handy here!
-
-   I abstracted out the code to take one single line
-   and parse it into a hand:
-     deck_t * hand_from_string(const char * str, future_cards_t * fc)
-   and recommend you do the same.
-
- Note that most of the rest of the code assumes that a poker hand
- has AT LEAST 5 cards in it.  Your read_input function should enforce
- this requirement. If there are fewer than 5 cards, print
- a useful error message and exit.
-*/
