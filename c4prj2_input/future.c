a5f3917177153a02409e76b42bb37b4fd0212c04
Coursera Learner <yhkim5719@gmail.com>
2019-08-12 05:20:44 +0000

    .

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -21,9 +21,9 @@ void future_cards_from_deck (deck_t* deck, future_cards_t* fc) {
 		perror("Need more cards in deck");
 		return;
 	}
-	for (int i = 0; i < fc->n_decks; i++) {
+	for (size_t i = 0; i < fc->n_decks; i++) {
 		if (fc->decks[i].n_cards == 0) {continue;}
-		for (int j = 0; j < fc->decks[i].n_cards; j++) {
+		for (size_t j = 0; j < fc->decks[i].n_cards; j++) {
 			fc->decks[i].cards[j]->suit = deck->cards[i]->suit;
 			fc->decks[i].cards[j]->value = deck->cards[i]->value;
 		}
