1e86204dce7a16d752efee50f961cf5cc211d5ed
Coursera Learner <yhkim5719@gmail.com>
2019-08-12 05:50:37 +0000

    .

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -9,6 +9,116 @@ int com (card_t c1,card_t c2){
 }
 
 void print_hand(deck_t * hand){
+  card_t ** card =hand -> cards ;
+  card_t  card1;
+  for (size_t i=0 ;i<(hand -> n_cards );i++){
+    card1=**(card +i);
+    print_card(card1);
+  }
+}
+
+int deck_contains(deck_t * d, card_t c) {
+  card_t ** card =d -> cards ;
+  for (size_t i=0 ;i< d -> n_cards ;i++){
+    if (com(**(card+i),c)) return 1;
+  }
+  return 0;
+}
+
+void shuffle(deck_t * d){
+  card_t ** card =d -> cards ;
+  card_t * temp;
+  size_t n=d ->n_cards;
+  int randarry;
+  for (size_t i=0 ;i< n/2 ;i++){
+    randarry= random()%(n-i)+i;
+    temp=card[i];
+    card[i]=card[randarry];
+    card[randarry]=temp;
+  }  
+}
+
+void assert_full_deck(deck_t * d) {
+  card_t ** card =d -> cards ;
+  card_t c;
+  int count;
+  for (size_t i=0 ;i< d -> n_cards ;i++){
+    c=**(card+i);
+    count=0;
+    for (size_t j=0 ;j< d -> n_cards ;j++){
+      if(com(**(card+j), c)) count ++;
+    }
+    assert(count ==1);
+  }
+}
+
+void add_card_to(deck_t * deck, card_t c){
+  card_t* c1=malloc(sizeof(*c1));
+  c1->value = c.value;
+  c1->suit = c.suit;
+  deck ->cards= realloc(deck->cards,(deck ->n_cards + 1)*sizeof(*deck ->cards));
+  deck ->cards[deck ->n_cards]=c1;
+  deck ->n_cards ++;
+}
+
+
+
+card_t * add_empty_card(deck_t * deck){
+  card_t* c1=malloc(sizeof(*c1));
+  c1->value = 0;
+  c1->suit = 0;
+  deck ->cards= realloc(deck->cards,(deck ->n_cards + 1)*sizeof(*deck ->cards));
+  deck ->cards[deck ->n_cards]=c1;
+  deck ->n_cards ++;
+  return c1;
+}
+
+
+
+
+
+deck_t * make_deck_exclude(deck_t * excluded_cards){
+  deck_t* ans=malloc(sizeof(*ans));
+  ans->cards=NULL;
+  ans->n_cards=0;
+  for(unsigned i=0;i<52;i++){
+    card_t c= card_from_num(i);
+    if(!deck_contains(excluded_cards,c)) add_card_to(ans,c);
+  }
+  return ans;
+}
+
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) {
+  deck_t* deck=malloc(sizeof(*deck));
+  deck->n_cards=0;
+  deck->cards=NULL;
+  for (size_t i=0 ; i<n_hands ;i++){
+    for(int x=0 ; x<hands[i]->n_cards ;x++){
+      deck->cards=realloc(deck->cards,(deck->n_cards+1)*sizeof(*deck->cards));
+      deck->cards[deck->n_cards]=hands[i]->cards[x];
+      deck->n_cards ++;
+    }
+  }
+  deck_t* deck2=make_deck_exclude(deck);
+  free(deck->cards);
+  free(deck);
+  return deck2 ;
+}
+
+
+
+void free_deck(deck_t * deck) {
+  for(int i=0 ;i<deck->n_cards;i++){
+    free(deck->cards[i]); 
+  }
+  free(deck->cards);
+  free(deck);  
+}
+
+
+
+/*
+void print_hand(deck_t * hand){
 	for (int i = 0; i < hand->n_cards; ++i) {
 		print_card(*(hand->cards[i]));
 //		if (i != hand->n_cards) {
@@ -24,8 +134,9 @@ int deck_contains(deck_t * d, card_t c) {
 	}
   	return 0;
 }
-
-/*	if (d->n_cards != 0) {
+*/
+/*
+	if (d->n_cards != 0) {
 		for (int i = 0; i < d->n_cards; ++i) {
 			if (d->cards[i]->value == c.value && d->cards[i]->suit == c.suit) {
 				return 1;
@@ -35,7 +146,7 @@ int deck_contains(deck_t * d, card_t c) {
   	return 0;
 }
 */
-
+/*
 void shuffle(deck_t * d){
 	for (int i = 0; i < d->n_cards; ++i) {
 		int j = rand();
@@ -119,3 +230,4 @@ void free_deck(deck_t* deck) {
 	free(deck->cards);
 	free(deck);
 }
+*/
