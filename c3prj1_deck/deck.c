ef7b5f5fd89de8af14eb8a6ebab871478678e619
Coursera Learner <yhkim5719@gmail.com>
2019-08-12 18:55:17 +0000

    .

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -1,4 +1,4 @@
-/*
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -164,8 +164,8 @@ void free_deck(deck_t * deck) {
   
 }
 
-*/
 
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -272,3 +272,4 @@ void free_deck(deck_t* deck) {
 	free(deck->cards);
 	free(deck);
 }
+*/
