1458a6edccb8f1218b5dc2cefa5acdf7fe9ca1bf
Coursera Learner <yhkim5719@gmail.com>
2019-08-12 05:09:39 +0000

    .

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
+
+int com (card_t c1,card_t c2){
+	if ((c1.value == c2.value) &&(c1.suit == c2.suit)) return 1;
+	return 0;
+}
+
 void print_hand(deck_t * hand){
 	for (int i = 0; i < hand->n_cards; ++i) {
 		print_card(*(hand->cards[i]));
